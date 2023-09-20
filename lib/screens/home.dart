7aa76e4983b0b5867c7d2d193928418d9bea33fd
Matheus Quost <matheusquost@gmail.com>
2023-09-20 06:12:51 -0300

    ✨ Implementing the search movies by category feature

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -36,26 +36,37 @@ class _HomeState extends State<Home> {
                   style: Theme.of(context).textTheme.displayLarge,
                 ),
               ),
-              const GenreFilter(),
+              GenreFilter(homeCubit: homeCubit,),
               BlocBuilder<HomeCubit, HomeStates>(
                 bloc: homeCubit,
                 builder: (context, state) {
                   if(state is HomeLoading) {
                     return const SliverFillRemaining(child: Center(child: CircularProgressIndicator(),));
                   } else if(state is HomeSuccess) {
-                    return SliverGrid.builder(
-                      gridDelegate:
-                          const SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 2,
-                        crossAxisSpacing: 16,
-                        mainAxisExtent: 240,
-                      ),
-                      itemBuilder: (context, index) {
-                        return MovieCard(
-                            movie: state.movies[index]);
-                      },
-                      itemCount: state.movies.length,
-                    );
+                    if(state.movies.isEmpty) {
+                      return SliverFillRemaining(child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const <Widget>[
+                          Icon(Icons.not_interested, size: 30.0,),
+                          SizedBox(height: 16.0,),
+                          Text("Não existem filmes com este gênero!"),
+                        ],
+                      ),);
+                    } else {
+                      return SliverGrid.builder(
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          crossAxisSpacing: 16,
+                          mainAxisExtent: 240,
+                        ),
+                        itemBuilder: (context, index) {
+                          return MovieCard(
+                              movie: state.movies[index]);
+                        },
+                        itemCount: state.movies.length,
+                      );
+                    }
                   } else if(state is HomeError){
                     return SliverFillRemaining(child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,

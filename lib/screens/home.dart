36333aa3c86660707ee2b2efd95ae877a03fd210
Matheus Quost <matheusquost@gmail.com>
2023-09-20 05:57:58 -0300

    ✨ Integrating HomeCubit to the HomeScreen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,8 +1,10 @@
+import 'package:bilheteria_panucci/logic/cubit/home_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:bilheteria_panucci/components/classification.dart';
 import 'package:bilheteria_panucci/components/home/genre_filter.dart';
 import 'package:bilheteria_panucci/components/movie_card.dart';
 import 'package:bilheteria_panucci/models/movie.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -12,9 +14,11 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final HomeCubit homeCubit = HomeCubit();
 
   @override
   void initState() {
+    homeCubit.getMovies();
     super.initState();
   }
 
@@ -33,16 +37,37 @@ class _HomeState extends State<Home> {
                 ),
               ),
               const GenreFilter(),
-              SliverGrid.builder(
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 16,
-                  mainAxisExtent: 240,
-                ),
-                itemBuilder: (context, index) {
-                  return MovieCard(movie: Movie(name: "James Bond", classification: Classification.naoRecomendado12, duration: "1h 22min", sinopse: "James Bond é um agente", genre: "Suspense", imageURI: null, sessions: ["18:00"]));
+              BlocBuilder<HomeCubit, HomeStates>(
+                bloc: homeCubit,
+                builder: (context, state) {
+                  if(state is HomeLoading) {
+                    return const SliverFillRemaining(child: Center(child: CircularProgressIndicator(),));
+                  } else if(state is HomeSuccess) {
+                    return SliverGrid.builder(
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        crossAxisSpacing: 16,
+                        mainAxisExtent: 240,
+                      ),
+                      itemBuilder: (context, index) {
+                        return MovieCard(
+                            movie: state.movies[index]);
+                      },
+                      itemCount: state.movies.length,
+                    );
+                  } else if(state is HomeError){
+                    return SliverFillRemaining(child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        const Icon(Icons.not_interested, size: 30.0,),
+                        const SizedBox(height: 16.00,),
+                        Text(state.error)
+                      ],
+                    ));
+                  }
+                  return SliverToBoxAdapter(child: Container(),);
                 },
-                itemCount: 5,
               ),
             ],
           ),
